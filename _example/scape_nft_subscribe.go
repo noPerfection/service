f83d1ed01017499e1f9c7976941f8d386ef0a576
ahmetson <ahmetson@zoho.com>
2023-02-17 14:26:27 +0300

    Fix the example

--- _example/scape_nft_subscribe.go
+++ _example/scape_nft_subscribe.go
@@ -3,11 +3,11 @@ package main
 import (
 	"fmt"
 
+	"github.com/blocklords/gosds/app/env"
 	"github.com/blocklords/gosds/categorizer"
-	"github.com/blocklords/gosds/env"
+	"github.com/blocklords/gosds/common/topic"
 	"github.com/blocklords/gosds/sdk"
 	"github.com/blocklords/gosds/security"
-	"github.com/blocklords/gosds/topic"
 )
 
 func main() {
