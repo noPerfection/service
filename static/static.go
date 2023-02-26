1b7bf4343bcfd48e41d0ee577110ef32bf4b0c84
ahmetson <ahmetson@zoho.com>
2023-02-26 14:02:59 +0300

    Move all static service whitelisted accounts to its own file

--- static/static.go
+++ static/static.go
@@ -38,62 +38,12 @@ func Run(app_config *configuration.Config, db_connection *db.Database, v *vault.
 		panic(err)
 	}
 
-	// The list of whitelisted SDS Services that can access to SDS Static:
-	//
-	//  * Developer Gateway
-	//  * Gateway
-	//  * Categorizer
-	developer_gateway_env, err := service.New(service.DEVELOPER_GATEWAY, service.REMOTE)
-	if err != nil {
-		panic(static_env)
-	}
-	developer_gateway := account.NewService(developer_gateway_env)
-
-	gateway_env, err := service.New(service.GATEWAY, service.REMOTE)
-	if err != nil {
-		panic(gateway_env)
-	}
-	gateway := account.NewService(gateway_env)
-
-	categorizer_env, err := service.New(service.CATEGORIZER, service.REMOTE)
-	if err != nil {
-		panic(categorizer_env)
-	}
-	categorizer := account.NewService(categorizer_env)
-
-	bundle_env, err := service.New(service.BUNDLE, service.REMOTE)
+	whitelisted_services, err := get_whitelisted_services()
 	if err != nil {
 		panic(err)
 	}
-	bundle := account.NewService(bundle_env)
-
-	log_env, err := service.New(service.LOG, service.REMOTE)
-	if err != nil {
-		panic(err)
-	}
-	log := account.NewService(log_env)
-
-	reader_env, err := service.New(service.READER, service.REMOTE)
-	if err != nil {
-		panic(err)
-	}
-	reader := account.NewService(reader_env)
-
-	writer_env, err := service.New(service.WRITER, service.REMOTE)
-	if err != nil {
-		panic(err)
-	}
-	writer := account.NewService(writer_env)
-
-	// SDS Spaghetti fetches the network parameters from SDS Static
-	spaghetti_env, err := service.New(service.SPAGHETTI, service.REMOTE)
-	if err != nil {
-		panic(err)
-	}
-	spaghetti := account.NewService(spaghetti_env)
-
-	accounts := account.NewAccounts(developer_gateway, gateway, categorizer)
-	accounts = accounts.Add(bundle, log, reader, writer, spaghetti)
+	accounts := account.NewServices(whitelisted_services)
+	// pass to the security channel
 
 	err = controller.ReplyController(db_connection, commands, static_env, accounts)
 	if err != nil {
