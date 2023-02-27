775549f252f4ab050f6ffee9773bbbf1cf97e2f1
ahmetson <ahmetson@zoho.com>
2023-02-27 16:53:09 +0300

    run evm clients during blockchain package start

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -3,8 +3,8 @@ package blockchain
 import (
 	"fmt"
 
-	"github.com/blocklords/gosds/blockchain/evm/client"
-	"github.com/blocklords/gosds/blockchain/evm/worker"
+	evm_client "github.com/blocklords/gosds/blockchain/evm/client"
+	evm_worker "github.com/blocklords/gosds/blockchain/evm/worker"
 	"github.com/blocklords/gosds/blockchain/network"
 )
 
@@ -17,13 +17,20 @@ func StartWorkers() error {
 
 	for _, new_network := range networks {
 		if new_network.Type == network.EVM {
-			new_client, err := client.New(new_network)
+			new_client, err := evm_client.New(new_network)
 			if err != nil {
-				return fmt.Errorf("gosds/blockchain: failed to create client: %v", err)
+				return fmt.Errorf("gosds/blockchain: failed to create EVM client: %v", err)
 			}
 
-			new_worker := worker.New(new_client, nil, false)
+			new_worker := evm_worker.New(new_client, nil, false)
 			go new_worker.Sync()
+			// } else if new_network.Type == network.IMX {
+			// new_client, err := imx_client.New(new_network)
+			// if err != nil {
+			// return fmt.Errorf("gosds/blockchain: failed to create IMX client: %v", err)
+			// }
+
+			// new_worker := imx_worker.New()
 		}
 	}
 
