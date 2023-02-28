bf7fef727141be448a2a854adcc8544b41bf1ae3
ahmetson <ahmetson@zoho.com>
2023-02-28 16:29:23 +0300

    app/env format error

--- app/env/file.go
+++ app/env/file.go
@@ -1,19 +1,23 @@
 package env
 
 import (
+	"fmt"
+
 	"github.com/blocklords/gosds/app/argument"
 	"github.com/joho/godotenv"
 )
 
 // Load all .env files
 func LoadAnyEnv() error {
-
 	opts := argument.GetEnvPaths()
 
 	godotenv.Load()
 
 	if len(opts) > 0 {
-		return godotenv.Load(opts...)
+		err := godotenv.Load(opts...)
+		if err != nil {
+			return fmt.Errorf("godotenv.Load for paths %v: %w", opts, err)
+		}
 	}
 	return nil
 }
