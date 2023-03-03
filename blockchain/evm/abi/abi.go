e1816b3627f91d1309544675c295a9c6c199278f
ahmetson <ahmetson@zoho.com>
2023-03-03 18:44:09 +0300

    Add decode function to the geth

--- blockchain/evm/abi/abi.go
+++ blockchain/evm/abi/abi.go
@@ -85,3 +85,58 @@ func NewAbi(static_abi *static_abi.Abi) (*Abi, error) {
 
 	return &abi_obj, nil
 }
+
+func get_indexed(inputs abi.Arguments) abi.Arguments {
+	ret := make(abi.Arguments, 0)
+	for _, arg := range inputs {
+		if arg.Indexed {
+			ret = append(ret, arg)
+		}
+	}
+	return ret
+}
+
+func (a *Abi) DecodeLog(topics []string, data string) (string, map[string]interface{}, error) {
+	if len(topics) == 0 {
+		return "", nil, fmt.Errorf("anonymous events are not supported")
+	}
+
+	topic_hashes := make([]eth_common.Hash, len(topics)-1)
+	var event_id eth_common.Hash
+	for i, topic := range topics {
+		if i == 0 {
+			event_id = eth_common.HexToHash(topic)
+		} else {
+			topic_hashes[i-1] = eth_common.HexToHash(topic)
+		}
+	}
+
+	topic_outputs := make(map[string]interface{}, 0)
+
+	data_outputs := make(map[string]interface{}, 0)
+	for _, event := range a.geth_abi.Events {
+		if strings.EqualFold(event_id.String(), event.ID.String()) {
+			if len(data) > 0 {
+				err := event.Inputs.NonIndexed().UnpackIntoMap(data_outputs, []byte(data))
+				if err != nil {
+					return "", nil, fmt.Errorf("parsing event %s for data %s error: %w", event.RawName, data, err)
+				}
+			}
+
+			indexed := get_indexed(event.Inputs)
+			err := abi.ParseTopicsIntoMap(topic_outputs, indexed, topic_hashes)
+			if err != nil {
+				return "", nil, fmt.Errorf("event %s for %v topics parsing error: %w", event.RawName, topics, err)
+			}
+
+			// merge topics and data
+			for key, value := range topic_outputs {
+				data_outputs[key] = value
+			}
+
+			return event.RawName, data_outputs, nil
+		}
+	}
+
+	return "", nil, fmt.Errorf("failed to decode the event. No matching signature")
+}
