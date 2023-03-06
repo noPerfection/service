2e68d6a5f2c1bac802b2ea7cb3009d323c23aaca
ahmetson <ahmetson@zoho.com>
2023-03-06 20:46:59 +0300

    Add static/configuration creation from topic

--- static/configuration/new.go
+++ static/configuration/new.go
@@ -4,7 +4,19 @@ import (
 	"fmt"
 
 	"github.com/blocklords/sds/common/data_type/key_value"
+	"github.com/blocklords/sds/common/topic"
 )
+
+// Converts the Topic to the Configuration
+func NewFromTopic(t topic.Topic) (*Configuration, error) {
+	return &Configuration{
+		Organization: t.Organization,
+		Project:      t.Project,
+		NetworkId:    t.NetworkId,
+		Group:        t.Group,
+		Name:         t.Smartcontract,
+	}, nil
+}
 
 // Creates a new static.Configuration class based on the given data
 func New(parameters key_value.KeyValue) (*Configuration, error) {
