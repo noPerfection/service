3be2d5f85aa5564cfd3c4fb59e3a3cd9e30400f7
ahmetson <ahmetson@zoho.com>
2023-03-10 20:26:30 +0300

    Move the fetching vault parameters to the service_type

--- app/service/service_type.go
+++ app/service/service_type.go
@@ -19,6 +19,16 @@ func (s ServiceType) ToString() string {
 	return string(s)
 }
 
+// Returns the Vault secret storage and the key for curve private part.
+func (name ServiceType) SecretKeyPath() (string, string) {
+	return "SDS_SERVICES", name.ToString() + "_SECRET_KEY"
+}
+
+// Returns the Vault secret path and the key for curve private part for broadcaster.
+func (name ServiceType) BroadcastSecretKeyPath() (string, string) {
+	return "SDS_SERVICES", name.ToString() + "_BROADCAST_SECRET_KEY"
+}
+
 func service_types() []ServiceType {
 	return []ServiceType{
 		CORE,
