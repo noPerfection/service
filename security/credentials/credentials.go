2c876ba8aaad08cfcfc7aa1037522ecfaeae7665
ahmetson <ahmetson@zoho.com>
2023-03-10 20:25:49 +0300

    Add security/Credentials

---
+++ security/credentials/credentials.go
@@ -0,0 +1,42 @@
+package credentials
+
+import (
+	"fmt"
+
+	"github.com/blocklords/sds/security/vault"
+
+	zmq "github.com/pebbe/zmq4"
+)
+
+type Credentials struct {
+	public_key  string
+	private_key string
+}
+
+// Fetch the given credentials from the Vault.
+// It fetches the private key from the vault.
+// Then gets the public key from it
+func NewFromVault(bucket string, key string) (*Credentials, error) {
+	private_key, err := vault.GetStringFromVault(bucket, key)
+	if err != nil {
+		return nil, fmt.Errorf("vault: %w", err)
+	}
+
+	pub_key, err := zmq.AuthCurvePublic(private_key)
+	if err != nil {
+		return nil, fmt.Errorf("zmq.Convert Secret to Pub: %w", err)
+	}
+
+	return &Credentials{
+		private_key: private_key,
+		public_key:  pub_key,
+	}, nil
+}
+
+// Returns the credentials but with public key only
+func New(public_key string) *Credentials {
+	return &Credentials{
+		public_key:  public_key,
+		private_key: "",
+	}
+}
