186e60abe12908a9888a78730bd601c33ea6a294
ahmetson <ahmetson@zoho.com>
2023-03-10 17:17:39 +0300

    add categorizer/event.RemoteSnapshot

--- categorizer/event/remote.go
+++ categorizer/event/remote.go
@@ -5,8 +5,44 @@ import (
 
 	"github.com/blocklords/sds/app/remote"
 	"github.com/blocklords/sds/app/remote/message"
+	"github.com/blocklords/sds/common/blockchain"
 	"github.com/blocklords/sds/common/data_type/key_value"
+	"github.com/blocklords/sds/common/smartcontract_key"
 )
+
+// Returns list of logs for the given smartcontract keys
+func RemoteSnapshot(socket *remote.Socket, smartcontract_keys []smartcontract_key.Key, block_timestamp blockchain.Timestamp) ([]*Log, blockchain.Timestamp, error) {
+	// Send hello.
+	request := message.Request{
+		Command:    "snapshot_get",
+		Parameters: key_value.Empty().Set("block_timestamp", block_timestamp).Set("smartcontract_keys", smartcontract_keys),
+	}
+	parameters, err := socket.RequestRemoteService(&request)
+	if err != nil {
+		return nil, 0, fmt.Errorf("snapshot_get remote request: %w", err)
+	}
+
+	raw_logs, err := parameters.GetKeyValueList("logs")
+	if err != nil {
+		return nil, 0, fmt.Errorf("GetKeyValueList(`logs`): %w", err)
+	}
+
+	logs := make([]*Log, len(raw_logs))
+	for i, raw := range raw_logs {
+		log, err := NewFromMap(raw)
+		if err != nil {
+			return nil, 0, fmt.Errorf("raw_log[%d] converting to Log: %w", i, err)
+		}
+		logs[i] = log
+	}
+
+	block_timestamp_to, err := blockchain.NewTimestampFromKeyValueParameter(parameters)
+	if err != nil {
+		return nil, 0, fmt.Errorf("block timestamp from reply parameter: %w", err)
+	}
+
+	return logs, block_timestamp_to, nil
+}
 
 // Return list of logs for the transaction keys from the remote SDS Categorizer.
 // For the transaction keys see
