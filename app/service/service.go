cc08c5cacb38a1af8574f2b92779a1e56e786acb
ahmetson <ahmetson@zoho.com>
2023-03-10 20:45:00 +0300

    Service curve keys moved to the credentials

--- app/service/service.go
+++ app/service/service.go
@@ -4,47 +4,16 @@ import (
 	"fmt"
 
 	"github.com/blocklords/sds/app/configuration"
-	"github.com/blocklords/sds/security/vault"
-
-	zmq "github.com/pebbe/zmq4"
+	"github.com/blocklords/sds/security/credentials"
 )
 
 // Environment variables for each SDS Service
 type Service struct {
-	Name               string // Service name
-	PublicKey          string // The Curve key of the service
-	SecretKey          string // The Curve secret key of the service
-	BroadcastPublicKey string
-	BroadcastSecretKey string
-	inproc             bool
-	url                string
-	broadcast_url      string
-}
-
-func (p *Service) set_curve_key(secret_key string) error {
-	p.SecretKey = secret_key
-
-	pub_key, err := zmq.AuthCurvePublic(secret_key)
-	if err != nil {
-		return fmt.Errorf("zmq.Convert Secret to Pub: %w", err)
-	}
-
-	p.PublicKey = pub_key
-
-	return nil
-}
-
-func (p *Service) set_broadcast_curve_key(secret_key string) error {
-	p.BroadcastSecretKey = secret_key
-
-	pub_key, err := zmq.AuthCurvePublic(secret_key)
-	if err != nil {
-		return fmt.Errorf("zmq.Convert Secret to Pub: %w", err)
-	}
-
-	p.BroadcastPublicKey = pub_key
-
-	return nil
+	Name          string // Service name
+	Credentials   *credentials.Credentials
+	inproc        bool
+	url           string
+	broadcast_url string
 }
 
 // Creates the service with the parameters but without any information
@@ -52,14 +21,10 @@ func Inprocess(service_type ServiceType) (*Service, error) {
 	name := string(service_type)
 
 	s := Service{
-		Name:               name,
-		inproc:             true,
-		url:                "inproc://reply_" + name,
-		broadcast_url:      "inproc://pub_" + name,
-		PublicKey:          "",
-		SecretKey:          "",
-		BroadcastPublicKey: "",
-		BroadcastSecretKey: "",
+		Name:          name,
+		inproc:        true,
+		url:           "inproc://reply_" + name,
+		broadcast_url: "inproc://pub_" + name,
 	}
 
 	return &s, nil
@@ -77,12 +42,8 @@ func NewExternal(service_type ServiceType, limit Limit) (*Service, error) {
 	broadcast_port_env := name + "_BROADCAST_PORT"
 
 	s := Service{
-		Name:               name,
-		inproc:             false,
-		PublicKey:          "",
-		SecretKey:          "",
-		BroadcastPublicKey: "",
-		BroadcastSecretKey: "",
+		Name:   name,
+		inproc: false,
 	}
 
 	switch limit {
@@ -114,58 +75,39 @@ func NewSecure(service_type ServiceType, limit Limit) (*Service, error) {
 		return nil, fmt.Errorf("service.New: %w", err)
 	}
 
-		case REMOTE:
-			if !app_config.Exist(public_key) {
-				return nil, fmt.Errorf("security enabled, but missing %s", s.Name)
-			}
-			s.PublicKey = app_config.GetString(public_key)
-		case THIS:
-			bucket, key_name := s.SecretKeyVariable()
-
-			SecretKey, err := vault.GetStringFromVault(bucket, key_name)
-			if err != nil {
-				return nil, fmt.Errorf("vault.GetString for %s service secret key: %w", s.Name, err)
-			}
-
-			if err := s.set_curve_key(SecretKey); err != nil {
-				return nil, fmt.Errorf("socket.set_curve_key %s: %w", s.Name, err)
-			}
-		case SUBSCRIBE:
-			if !app_config.Exist(broadcast_public_key) {
-				return nil, fmt.Errorf("security enabled, but missing %s", s.Name)
-			}
-			s.BroadcastPublicKey = app_config.GetString(broadcast_public_key)
-		case BROADCAST:
-			bucket, key_name := s.BroadcastSecretKeyVariable()
-			SecretKey, err := vault.GetStringFromVault(bucket, key_name)
-			if err != nil {
-				return nil, fmt.Errorf("vault.GetString for %s service broadcast secret key: %w", s.Name, err)
-
-			}
-
-			if err := s.set_broadcast_curve_key(SecretKey); err != nil {
-				return nil, fmt.Errorf("socket.set_broadcast_curve_key %s: %w", s.Name, err)
-			}
 	switch limit {
+	case REMOTE:
+		if !app_config.Exist(public_key) {
+			return nil, fmt.Errorf("security enabled, but missing %s", s.Name)
 		}
+		s.Credentials = credentials.New(public_key)
+	case THIS:
+		bucket, key_name := service_type.SecretKeyPath()
 
-	return s, nil
-}
-
-
-// Returns the service environment parameters by its Public Key
-func GetByPublicKey(PublicKey string) (*Service, error) {
-	for _, service_type := range service_types() {
-		service, err := NewSecure(service_type, THIS)
+		creds, err := credentials.NewFromVault(bucket, key_name)
 		if err != nil {
-			return nil, fmt.Errorf("service.New(`%s`): %w", service_type, err)
+			return nil, fmt.Errorf("vault.GetString for %s service secret key: %w", s.Name, err)
 		}
-		if service != nil && service.PublicKey == PublicKey {
-			return service, nil
+
+		s.Credentials = creds
+	case SUBSCRIBE:
+		if !app_config.Exist(broadcast_public_key) {
+			return nil, fmt.Errorf("security enabled, but missing %s", s.Name)
 		}
+
+		s.Credentials = credentials.New(app_config.GetString(broadcast_public_key))
+	case BROADCAST:
+		bucket, key_name := service_type.BroadcastSecretKeyPath()
+
+		creds, err := credentials.NewFromVault(bucket, key_name)
+		if err != nil {
+			return nil, fmt.Errorf("vault.GetString for %s service secret key: %w", s.Name, err)
+		}
+
+		s.Credentials = creds
 	}
 
-	return nil, fmt.Errorf("public key '%s' not matches to any service", PublicKey)
+	return s, nil
 }
 
 // Returns the request-reply url as a host:port
