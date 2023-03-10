7ccd4df51c395cae2800dd6ccbd46440a8f28261
ahmetson <ahmetson@zoho.com>
2023-03-10 21:08:05 +0300

    Account doesn't support service accounts

--- app/account/account.go
+++ app/account/account.go
@@ -4,20 +4,17 @@ package account
 import (
 	"fmt"
 
-	"github.com/blocklords/sds/app/service"
 	"github.com/blocklords/sds/common/data_type/key_value"
 )
 
 // Requester to the SDS Service. It's either a developer or another SDS service.
 type Account struct {
-	Id             uint64           `json:"id,omitempty"`    // Auto incremented for every new developer
-	PublicKey      string           `json:"public_key"`      // Public Key for authentication.
-	Organization   string           `json:"organization"`    // Organization
-	NonceTimestamp uint64           `json:"nonce_timestamp"` // Nonce since the last usage. Only acceptable for developers
-	service        *service.Service // If the account is another service, then this parameter keeps the data. Otherwise this parameter is a nil.
+	Id             uint64 `json:"id,omitempty"`    // Auto incremented for every new developer
+	PublicKey      string `json:"public_key"`      // Public Key for authentication.
+	Organization   string `json:"organization"`    // Organization
+	NonceTimestamp uint64 `json:"nonce_timestamp"` // Nonce since the last usage. Only acceptable for developers
 }
 
-// List of accounts to manipulate with
 type Accounts []*Account
 
 // Creates the account from the public key
@@ -34,38 +31,9 @@ func NewDeveloper(id uint64, public_key string, nonce_timestamp uint64, organiza
 		PublicKey:      public_key,
 		NonceTimestamp: nonce_timestamp,
 		Organization:   organization,
-		service:        nil,
 	}
 }
 
-// Creates a new Account for a service
-func NewService(service *service.Service) *Account {
-	return &Account{
-		Id:             0,
-		NonceTimestamp: 0,
-		PublicKey:      service.Credentials.PublicKey,
-		Organization:   "",
-		service:        service,
-	}
-}
-
-// Creates an account for a service
-func NewServices(services []*service.Service) Accounts {
-	accounts := make(Accounts, len(services))
-	for i, s := range services {
-		accounts[i] = NewService(s)
-	}
-
-	return accounts
-}
-
-func (account *Account) IsDeveloper() bool {
-	return account.service == nil
-}
-
-func (account *Account) IsService() bool {
-	return account.service != nil
-}
 
 func ParseJson(raw key_value.KeyValue) (*Account, error) {
 	public_key, err := raw.GetString("public_key")
@@ -141,39 +109,4 @@ func (accounts Accounts) Remove(new_accounts ...*Account) Accounts {
 	}
 
 	return accounts
-}
-
-func (accounts Accounts) PublicKeys() []string {
-	public_keys := make([]string, len(accounts))
-
-	for i := range accounts {
-		public_keys[i] = accounts[i].PublicKey
-	}
-
-	return public_keys
-}
-
-func (accounts Accounts) BroadcastPublicKeys() []string {
-	public_keys := make([]string, len(accounts))
-
-	for i := range accounts {
-		public_keys[i] = accounts[i].service.Credentials.PublicKey
-	}
-
-	return public_keys
-}
-
-// Returns the names of the account users
-func (accounts Accounts) Names() []string {
-	names := make([]string, 0)
-
-	for i := range accounts {
-		if accounts[i].IsDeveloper() {
-			names[i] = fmt.Sprintf("developer %s from %s organization", accounts[i].PublicKey, accounts[i].Organization)
-		} else {
-			names[i] = accounts[i].service.Name
-		}
-	}
-
-	return names
 }
