c4ae92a5496d2485fbd1b0dbdf1c7f11dfeaf6d6
ahmetson <ahmetson@zoho.com>
2023-03-10 16:48:41 +0300

    Move the registration of a new smartcontract to the handler package

--- categorizer/categorizer.go
+++ categorizer/categorizer.go
@@ -11,7 +11,6 @@ import (
 	"github.com/blocklords/sds/blockchain/network"
 	"github.com/blocklords/sds/categorizer/handler"
 	"github.com/blocklords/sds/categorizer/smartcontract"
-	"github.com/blocklords/sds/common/data_type/key_value"
 	static_abi "github.com/blocklords/sds/static/abi"
 
 	"github.com/blocklords/sds/app/configuration"
@@ -29,12 +28,14 @@ import (
 
 var static_socket *remote.Socket
 
-// Manages the EVM based smartcontracts on a certain blockchain
-// todo use the blockchain/categorizer_push(network_id); defer close()
-// then to categorizer_request.add_smartcontract(smartcontract)
+// Sends the smartcontracts to the blockchain package.
+//
+// The blockchain package will have the categorizer for its each blockchain type.
+// They will handle the decoding the event logs.
+// After decoding, the blockchain/categorizer will push back to this categorizer's puller.
 func setup_smartcontracts(logger log.Logger, db_con *db.Database, network *network.Network) error {
 	var mu sync.Mutex
-	logger.Info("get all smartcontracts from database", "network_id", network.Id)
+	logger.Info("get all categorizable smartcontracts from database", "network_id", network.Id)
 	smartcontracts, err := smartcontract.GetAllByNetworkId(db_con, network.Id)
 	if err != nil {
 		return fmt.Errorf("smartcontract.GetAllByNetworkId: %w", err)
@@ -84,73 +85,6 @@ func setup_smartcontracts(logger log.Logger, db_con *db.Database, network *netwo
 	return nil
 }
 
-////////////////////////////////////////////////////////////////////
-//
-// Command handlers
-//
-////////////////////////////////////////////////////////////////////
-
-// Saves the smartcontract in the database.
-// then start a worker.
-func smartcontract_set(request message.Request, logger log.Logger, parameters ...interface{}) message.Reply {
-	db_con := parameters[0].(*db.Database)
-
-	kv, err := request.Parameters.GetKeyValue("smartcontract")
-	if err != nil {
-		return message.Fail("missing 'smartcontract' parameter")
-	}
-
-	sm, err := smartcontract.New(kv)
-	if err != nil {
-		return message.Fail("request parameter -> smartcontract.New: " + err.Error())
-	}
-
-	if smartcontract.Exists(db_con, sm.Key) {
-		return message.Fail("the smartcontract already in SDS Categorizer")
-	}
-
-	saveErr := smartcontract.Save(db_con, sm)
-	if saveErr != nil {
-		return message.Fail("database: " + saveErr.Error())
-	}
-
-	pusher, err := blockchain_proc.CategorizerManagerSocket(sm.Key.NetworkId)
-	if err != nil {
-		return message.Fail("inproc: " + err.Error())
-	}
-	defer pusher.Close()
-
-	remote_abi, err := static_abi.Get(static_socket, sm.Key)
-	if err != nil {
-		return message.Fail("failed to set the ABI from SDS Static. This is an exception. It should not happen. error: " + err.Error())
-	}
-
-	smartcontracts := []*smartcontract.Smartcontract{sm}
-	static_abis := []*static_abi.Abi{remote_abi}
-
-	push := message.Request{
-		Command: "new-smartcontracts",
-		Parameters: map[string]interface{}{
-			"smartcontracts": smartcontracts,
-			"abis":           static_abis,
-		},
-	}
-	request_string, _ := push.ToString()
-
-	_, err = pusher.SendMessage(request_string)
-	if err != nil {
-		return message.Fail("send: " + err.Error())
-	}
-
-	reply := message.Reply{
-		Status:     "OK",
-		Message:    "",
-		Parameters: key_value.Empty().Set("smartcontract", sm),
-	}
-
-	return reply
-}
-
 // This core service decodes the smartcontract event logs.
 // The event data stored in the database.
 // Provides commands to fetch the decoded logs from SDK.
@@ -192,10 +126,9 @@ func Run(app_config *configuration.Config, db_con *db.Database) {
 	var commands = controller.CommandHandlers{
 		"smartcontract_get_all": handler.GetSmartcontracts,
 		"smartcontract_get":     handler.GetSmartcontract,
+		"smartcontract_set":     handler.SetSmartcontract,
 
 		"snapshot_get": handler.GetSnapshot,
-
-		"smartcontract_set": smartcontract_set,
 	}
 
 	reply, err := controller.NewReply(categorizer_env)
--- categorizer/handler/smartcontract.go
+++ categorizer/handler/smartcontract.go
@@ -3,16 +3,20 @@ package handler
 import (
 	"github.com/charmbracelet/log"
 
+	blockchain_process "github.com/blocklords/sds/blockchain/inproc"
 	"github.com/blocklords/sds/categorizer/smartcontract"
 	"github.com/blocklords/sds/db"
+	static_abi "github.com/blocklords/sds/static/abi"
 
+	"github.com/blocklords/sds/app/remote"
 	"github.com/blocklords/sds/app/remote/message"
+	"github.com/blocklords/sds/app/service"
 	"github.com/blocklords/sds/common/data_type"
 	"github.com/blocklords/sds/common/data_type/key_value"
 	"github.com/blocklords/sds/common/smartcontract_key"
 )
 
-// return a categorizer block by network id and smartcontract address
+// return a categorized smartcontract parameters by network id and smartcontract address
 func GetSmartcontract(request message.Request, logger log.Logger, parameters ...interface{}) message.Reply {
 	db := parameters[0].(*db.Database)
 
@@ -48,6 +52,74 @@ func GetSmartcontracts(_ message.Request, logger log.Logger, parameters ...inter
 		Status:     "OK",
 		Message:    "",
 		Parameters: key_value.Empty().Set("smartcontracts", data_type.ToMapList(smartcontracts)),
+	}
+
+	return reply
+}
+
+// Register a new smartcontract to categorizer.
+func SetSmartcontract(request message.Request, logger log.Logger, parameters ...interface{}) message.Reply {
+	db_con := parameters[0].(*db.Database)
+
+	kv, err := request.Parameters.GetKeyValue("smartcontract")
+	if err != nil {
+		return message.Fail("missing 'smartcontract' parameter")
+	}
+
+	sm, err := smartcontract.New(kv)
+	if err != nil {
+		return message.Fail("request parameter -> smartcontract.New: " + err.Error())
+	}
+
+	if smartcontract.Exists(db_con, sm.Key) {
+		return message.Fail("the smartcontract already in SDS Categorizer")
+	}
+
+	saveErr := smartcontract.Save(db_con, sm)
+	if saveErr != nil {
+		return message.Fail("database: " + saveErr.Error())
+	}
+
+	pusher, err := blockchain_process.CategorizerManagerSocket(sm.Key.NetworkId)
+	if err != nil {
+		return message.Fail("inproc: " + err.Error())
+	}
+	defer pusher.Close()
+
+	categorizer_env, _ := service.Inprocess(service.CATEGORIZER)
+	static_env, err := service.Inprocess(service.STATIC)
+	if err != nil {
+		logger.Fatal("new static service config", "message", err)
+	}
+	static_socket := remote.TcpRequestSocketOrPanic(static_env, categorizer_env)
+	defer static_socket.Close()
+
+	remote_abi, err := static_abi.Get(static_socket, sm.Key)
+	if err != nil {
+		return message.Fail("failed to set the ABI from SDS Static. This is an exception. It should not happen. error: " + err.Error())
+	}
+
+	smartcontracts := []*smartcontract.Smartcontract{sm}
+	static_abis := []*static_abi.Abi{remote_abi}
+
+	push := message.Request{
+		Command: "new-smartcontracts",
+		Parameters: map[string]interface{}{
+			"smartcontracts": smartcontracts,
+			"abis":           static_abis,
+		},
+	}
+	request_string, _ := push.ToString()
+
+	_, err = pusher.SendMessage(request_string)
+	if err != nil {
+		return message.Fail("send: " + err.Error())
+	}
+
+	reply := message.Reply{
+		Status:     "OK",
+		Message:    "",
+		Parameters: key_value.Empty().Set("smartcontract", sm),
 	}
 
 	return reply
