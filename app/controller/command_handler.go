bc32038cc1344dcfa21ef399939bda62d2efda86
ahmetson <ahmetson@zoho.com>
2023-03-13 14:14:50 +0300

    Add a function to CommandHandlers that returns list of command names

--- app/controller/command_handler.go
+++ app/controller/command_handler.go
@@ -13,3 +13,16 @@ func (c CommandHandlers) Exist(command string) bool {
 	_, ok := c[command]
 	return ok
 }
+
+// Returns the list of command names without handlers
+func (c CommandHandlers) Commands() []string {
+	commands := make([]string, len(c))
+
+	i := 0
+	for name := range c {
+		commands[i] = name
+		i++
+	}
+
+	return commands
+}
