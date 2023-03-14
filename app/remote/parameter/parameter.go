b25500f4257a86eee574d4fe2cf32b4c6daf3296
ahmetson <ahmetson@zoho.com>
2023-03-14 12:40:43 +0300

    add request attempts (to avoid request handling forever)

--- app/remote/parameter/parameter.go
+++ app/remote/parameter/parameter.go
@@ -10,6 +10,7 @@ import (
 // This is the default time if argument wasn't given that changes the REQUEST_TIMEOUT
 const (
 	REQUEST_TIMEOUT = 30 * time.Second //  msecs, (> 1000!)
+	ATTEMPT         = uint(5)
 )
 
 // Request timeout
@@ -24,4 +25,18 @@ func RequestTimeout() time.Duration {
 	}
 
 	return request_timeout
+}
+
+// How many attempts we make
+func Attempt() uint {
+	attempt := ATTEMPT
+	app_config := configuration.New()
+	if app_config.Exist("SDS_REQUEST_ATTEMPT") {
+		env_attempt := app_config.GetUint64("SDS_REQUEST_ATTEMPT")
+		if env_attempt != 0 {
+			attempt = uint(env_attempt)
+		}
+	}
+
+	return attempt
 }
