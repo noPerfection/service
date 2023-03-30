1cd53d7f5941c33b6214a68f0a5107d59ecc761b
ahmetson <ahmetson@zoho.com>
2023-03-30 17:31:53 +0300

    pick only .env ended environment variables from the argument

--- app/configuration/env/file.go
+++ app/configuration/env/file.go
@@ -7,17 +7,33 @@ import (
 	"github.com/joho/godotenv"
 )
 
-// Load all .env files
+// Load all *.env files
 func LoadAnyEnv() error {
 	opts := argument.GetEnvPaths()
 
-	godotenv.Load()
+	if len(opts) == 0 {
+		return nil
+	}
 
-	if len(opts) > 0 {
-		err := godotenv.Load(opts...)
-		if err != nil {
-			return fmt.Errorf("godotenv.Load for paths %v: %w", opts, err)
+	paths := make([]string, 0)
+	for _, opt := range opts {
+		if len(opt) < 4 {
+			continue
 		}
+
+		last_part := opt[len(opt)-4:]
+		if last_part == ".env" {
+			paths = append(paths, opt)
+		}
+	}
+
+	if len(paths) == 0 {
+		return nil
+	}
+
+	err := godotenv.Load(paths...)
+	if err != nil {
+		return fmt.Errorf("godotenv.Load for paths %v: %w", opts, err)
 	}
 	return nil
 }
