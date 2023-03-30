b6c8491233aad3b8c53187cffc233b6628ba80b9
ahmetson <ahmetson@zoho.com>
2023-03-30 20:00:42 +0300

    Add app/configuration tests

---
+++ app/configuration/env/file_test.go
@@ -0,0 +1,45 @@
+package env
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+// Define the suite, and absorb the built-in basic suite
+// functionality from testify - including a T() method which
+// returns the current testing context
+type TestEnvSuite struct {
+	suite.Suite
+	env_path string
+}
+
+// Make sure that Account is set to five
+// before each test
+func (suite *TestEnvSuite) SetupTest() {
+	suite.env_path = ".test.env"
+	os.Args = append(os.Args, suite.env_path)
+
+	file, err := os.Create(suite.env_path)
+	suite.Require().NoError(err)
+	_, err = file.WriteString("")
+	suite.Require().NoError(err, "failed to write the data into: "+suite.env_path)
+	err = file.Close()
+	suite.Require().NoError(err, "delete the dump file: "+suite.env_path)
+}
+
+// All methods that begin with "Test" are run as tests within a
+// suite.
+func (suite *TestEnvSuite) TestRun() {
+	err := LoadAnyEnv()
+	suite.Require().NoError(err)
+	err = os.Remove(suite.env_path)
+	suite.Require().NoError(err, "delete the dump file: "+suite.env_path)
+}
+
+// In order for 'go test' to run this suite, we need to create
+// a normal test function and pass our suite to suite.Run
+func TestCommand(t *testing.T) {
+	suite.Run(t, new(TestEnvSuite))
+}
