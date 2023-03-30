4711083b497ba1fddf4e6e72322d4754ada33c3e
ahmetson <ahmetson@zoho.com>
2023-03-30 17:59:31 +0300

    Verify that parameters of request data is correct

--- app/command/command.go
+++ app/command/command.go
@@ -69,6 +69,23 @@ func (command Command) Request(socket *remote.Socket, request interface{}, reply
 //
 // Both request and reply are the message parameters.
 func (command Command) Push(socket *zmq.Socket, request interface{}) error {
+	socket_type, err := socket.GetType()
+	if err != nil {
+		return fmt.Errorf("socket.GetType: %w", err)
+	}
+	if socket_type != zmq.PUSH {
+		return fmt.Errorf("socket type %s not supported. Only is supported PUSH", socket_type)
+	}
+
+	_, ok := request.(message.Request)
+	if ok {
+		return fmt.Errorf("the request can not be of message.Request type")
+	}
+	_, ok = request.(message.SmartcontractDeveloperRequest)
+	if ok {
+		return fmt.Errorf("the request can not be of message.SmartcontractDeveloperRequest type")
+	}
+
 	var mu sync.Mutex
 	request_parameters, err := key_value.NewFromInterface(request)
 	if err != nil {
@@ -96,6 +113,24 @@ func (command Command) Push(socket *zmq.Socket, request interface{}) error {
 }
 
 func (command Command) RequestRouter(socket *remote.Socket, service_type service.ServiceType, request interface{}, reply interface{}) error {
+	_, ok := request.(message.Request)
+	if ok {
+		return fmt.Errorf("the request can not be of message.Request type")
+	}
+	_, ok = request.(message.SmartcontractDeveloperRequest)
+	if ok {
+		return fmt.Errorf("the request can not be of message.SmartcontractDeveloperRequest type")
+	}
+
+	_, ok = reply.(message.Reply)
+	if ok {
+		return fmt.Errorf("the reply can not be of message.Reply type")
+	}
+	_, ok = reply.(message.Broadcast)
+	if ok {
+		return fmt.Errorf("the reply can not be of message.Broadcast type")
+	}
+
 	request_parameters, err := key_value.NewFromInterface(request)
 	if err != nil {
 		return fmt.Errorf("conver parameters to: %w", err)
