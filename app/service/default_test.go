6866bb2782961d833373ac2652983061ddab4970
ahmetson <ahmetson@zoho.com>
2023-03-31 21:17:35 +0300

    add app/service test

---
+++ app/service/default_test.go
@@ -0,0 +1,44 @@
+package service
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+// Define the suite, and absorb the built-in basic suite
+// functionality from testify - including a T() method which
+// returns the current testing context
+type TestDefaultSuite struct {
+	suite.Suite
+}
+
+// Todo test inprocess and external types of controllers
+// Todo test the business of the controller
+// Make sure that Account is set to five
+// before each test
+func (suite *TestDefaultSuite) SetupTest() {
+	types := service_types()
+	suite.Require().Len(types, 9)
+	configs := DefaultConfigurations()
+	suite.Require().Len(configs, len(types))
+
+}
+
+func (suite *TestDefaultSuite) TestRandom() {
+	configs := DefaultConfigurations()
+	suite.Equal("CORE", configs[0].Title)
+	port, err := configs[2].Parameters.GetString("CATEGORIZER_PORT")
+	suite.Require().NoError(err)
+	suite.Equal("4020", port)
+
+	broadcast_port, err := configs[5].Parameters.GetString("DEVELOPER_GATEWAY_BROADCAST_PORT")
+	suite.Require().NoError(err)
+	suite.Equal("4051", broadcast_port)
+}
+
+// In order for 'go test' to run this suite, we need to create
+// a normal test function and pass our suite to suite.Run
+func TestDefault(t *testing.T) {
+	suite.Run(t, new(TestDefaultSuite))
+}
---
+++ app/service/service_type_test.go
@@ -0,0 +1,52 @@
+package service
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+// Define the suite, and absorb the built-in basic suite
+// functionality from testify - including a T() method which
+// returns the current testing context
+type TestServiceTypeSuite struct {
+	suite.Suite
+}
+
+// Todo test inprocess and external types of controllers
+// Todo test the business of the controller
+// Make sure that Account is set to five
+// before each test
+func (suite *TestServiceTypeSuite) SetupTest() {
+	service := BUNDLE
+
+	suite.Equal("BUNDLE", service.ToString())
+
+	bucket, name := service.SecretKeyPath()
+	suite.Equal(BUCKET, bucket)
+	suite.Equal("BUNDLE_SECRET_KEY", name)
+
+	broadcast_bucket, broadcast_name := service.BroadcastSecretKeyPath()
+	suite.Equal(BUCKET, broadcast_bucket)
+	suite.Equal("BUNDLE_BROADCAST_SECRET_KEY", broadcast_name)
+}
+
+func (suite *TestServiceTypeSuite) TestTypes() {
+	types := service_types()
+	suite.Require().Len(types, 9)
+	suite.Equal(CORE, types[0])
+	suite.Equal(SPAGHETTI, types[1])
+	suite.Equal(CATEGORIZER, types[2])
+	suite.Equal(STATIC, types[3])
+	suite.Equal(GATEWAY, types[4])
+	suite.Equal(DEVELOPER_GATEWAY, types[5])
+	suite.Equal(READER, types[6])
+	suite.Equal(WRITER, types[7])
+	suite.Equal(BUNDLE, types[8])
+}
+
+// In order for 'go test' to run this suite, we need to create
+// a normal test function and pass our suite to suite.Run
+func TestServiceType(t *testing.T) {
+	suite.Run(t, new(TestServiceTypeSuite))
+}
