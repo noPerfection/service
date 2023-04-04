55cc129c0a5ba8cdadc577c9b5afeda6bc289917
ahmetson <ahmetson@zoho.com>
2023-04-04 20:41:49 +0300

    change the varchar length for static_configuration

--- _db/migrations/20230308173943_static_configuration.sql
+++ _db/migrations/20230308173943_static_configuration.sql
@@ -1,11 +1,11 @@
 -- +goose Up
 CREATE TABLE static_configuration (
     network_id varchar(20) NOT NULL,
-	address varchar(255) NOT NULL, 
-	organization varchar(255) NOT NULL,
-    project varchar(255) NOT NULL,
+	address varchar(84) NOT NULL, 
+	organization varchar(127) NOT NULL,
+    project varchar(127) NOT NULL,
     group_name smallint unsigned,
-    smartcontract_name varchar(255) NOT NULL,
+    smartcontract_name varchar(127) NOT NULL,
     PRIMARY KEY (organization, project, network_id, group_name, smartcontract_name),
     CONSTRAINT smartcontract_id FOREIGN KEY (network_id, address) REFERENCES static_smartcontract(network_id, address) 
 );
--- _db/migrations/20230314150414_static_configuration_group_type.sql
+++ _db/migrations/20230314150414_static_configuration_group_type.sql
@@ -1,2 +1,2 @@
 -- +goose Up
-ALTER TABLE static_configuration MODIFY group_name varchar(50);
+ALTER TABLE static_configuration MODIFY group_name varchar(127);
