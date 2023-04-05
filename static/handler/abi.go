26b01d3f0c3f51b0d690993cfc3a5549288072fe
ahmetson <ahmetson@zoho.com>
2023-04-05 17:56:31 +0300

    ++ static/handler/abi validation

--- static/handler/abi.go
+++ static/handler/abi.go
@@ -13,29 +13,31 @@ import (
 type GetAbiRequest struct {
 	Id string `json:"abi_id"`
 }
+type GetAbiReply = abi.Abi
 
 type SetAbiRequest struct {
 	Body interface{} `json:"body"`
 }
-
-type GetAbiReply = abi.Abi
-type SetAbiReply = GetAbiReply
+type SetAbiReply = abi.Abi
 
 // Returns an abi by abi id
 func AbiGet(request message.Request, _ log.Logger, parameters ...interface{}) message.Reply {
 	var req_parameters GetAbiRequest
 	err := request.Parameters.ToInterface(&req_parameters)
 	if err != nil {
-		return message.Fail("failed to parse data")
+		return message.Fail("request.Parameters -> Command Parameter: " + err.Error())
 	}
 	if len(req_parameters.Id) == 0 {
 		return message.Fail("missing abi id")
 	}
 
-	abi_list := parameters[1].(*key_value.List)
+	abi_list, ok := parameters[1].(*key_value.List)
+	if !ok {
+		return message.Fail("missing abi list")
+	}
 	abi_raw, err := abi_list.Get(req_parameters.Id)
 	if err != nil {
-		return message.Fail("failed to get abi: " + err.Error())
+		return message.Fail("abi_list.Get: " + err.Error())
 	}
 
 	reply_message, err := command.Reply(abi_raw)
@@ -47,17 +49,22 @@ func AbiGet(request message.Request, _ log.Logger, parameters ...interface{}) me
 }
 
 func AbiRegister(request message.Request, _ log.Logger, parameters ...interface{}) message.Reply {
-	db_con := parameters[0].(*db.Database)
-
 	var request_parameters SetAbiRequest
 	err := request.Parameters.ToInterface(&request_parameters)
 	if err != nil {
 		return message.Fail("failed to parse data")
 	}
 
+	if request_parameters.Body == nil {
+		return message.Fail("missing body")
+	}
+
 	new_abi, err := abi.NewFromInterface(request_parameters.Body)
 	if err != nil {
-		return message.Fail(err.Error())
+		return message.Fail("abi.NewFromInterface: " + err.Error())
+	}
+	if len(new_abi.Bytes) == 0 {
+		return message.Fail("body is empty")
 	}
 
 	reply_message, err := command.Reply(new_abi)
@@ -65,10 +72,13 @@ func AbiRegister(request message.Request, _ log.Logger, parameters ...interface{
 		return message.Fail("failed to reply")
 	}
 
-	abi_list := parameters[1].(*key_value.List)
+	abi_list, ok := parameters[1].(*key_value.List)
+	if !ok {
+		return message.Fail("missing abi list")
+	}
 	_, err = abi_list.Get(new_abi.Id)
-	if err != nil {
-		return message.Fail("failed to get abi: " + err.Error())
+	if err == nil {
+		return message.Fail("abi registered already for")
 	}
 
 	err = abi_list.Add(new_abi.Id, new_abi)
@@ -76,9 +86,12 @@ func AbiRegister(request message.Request, _ log.Logger, parameters ...interface{
 		return message.Fail("failed to add abi to abi list: " + err.Error())
 	}
 
-	save_err := abi.SetInDatabase(db_con, new_abi)
-	if save_err != nil {
-		return message.Fail("database error:" + err.Error())
+	db_con, ok := parameters[0].(*db.Database)
+	if ok {
+		save_err := abi.SetInDatabase(db_con, new_abi)
+		if save_err != nil {
+			return message.Fail("database error:" + err.Error())
+		}
 	}
 
 	return reply_message
