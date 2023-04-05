55fb896d131fb5178c48c0595b80997d8271e209
ahmetson <ahmetson@zoho.com>
2023-04-05 18:32:41 +0300

    ++ static/handler/configuration validation

--- static/handler/configuration.go
+++ static/handler/configuration.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"github.com/blocklords/sds/app/log"
 	"github.com/blocklords/sds/common/data_type/key_value"
+	"github.com/blocklords/sds/common/topic"
 	"github.com/blocklords/sds/db"
 	"github.com/blocklords/sds/static/configuration"
 
@@ -10,17 +11,16 @@ import (
 	"github.com/blocklords/sds/app/remote/message"
 )
 
-type GetConfigurationRequest = configuration.Configuration
-type SetConfigurationRequest = configuration.Configuration
+type GetConfigurationRequest = topic.Topic
 type GetConfigurationReply = configuration.Configuration
+
+type SetConfigurationRequest = configuration.Configuration
 type SetConfigurationReply = configuration.Configuration
 
 // Register a new smartcontract in the configuration.
 // It requires smartcontract address. First call smartcontract_register command.
 func ConfigurationRegister(request message.Request, logger log.Logger, parameters ...interface{}) message.Reply {
-	db_con := parameters[0].(*db.Database)
-
-	var conf GetConfigurationRequest
+	var conf SetConfigurationRequest
 	err := request.Parameters.ToInterface(&conf)
 	if err != nil {
 		return message.Fail("failed to parse data")
@@ -29,7 +29,11 @@ func ConfigurationRegister(request message.Request, logger log.Logger, parameter
 		return message.Fail("validation: " + err.Error())
 	}
 
-	conf_list := parameters[3].(*key_value.List)
+	conf_list, ok := parameters[3].(*key_value.List)
+	if !ok {
+		return message.Fail("missing configuration list")
+	}
+
 	conf_key := conf.Topic.ToString(conf.Topic.Level())
 	_, err = conf_list.Get(conf_key)
 	if err != nil {
@@ -41,8 +45,11 @@ func ConfigurationRegister(request message.Request, logger log.Logger, parameter
 		return message.Fail("failed to add abi to abi list: " + err.Error())
 	}
 
-	if err = configuration.SetInDatabase(db_con, &conf); err != nil {
-		return message.Fail("Configuration saving in the database failed: " + err.Error())
+	db_con, ok := parameters[0].(*db.Database)
+	if ok {
+		if err = configuration.SetInDatabase(db_con, &conf); err != nil {
+			return message.Fail("Configuration saving in the database failed: " + err.Error())
+		}
 	}
 
 	var reply GetConfigurationReply = conf
@@ -56,17 +63,23 @@ func ConfigurationRegister(request message.Request, logger log.Logger, parameter
 
 // Returns configuration and smartcontract information related to the configuration
 func ConfigurationGet(request message.Request, _ log.Logger, parameters ...interface{}) message.Reply {
-	var conf GetConfigurationRequest
-	err := request.Parameters.ToInterface(&conf)
+	var conf_topic GetConfigurationRequest
+	err := request.Parameters.ToInterface(&conf_topic)
 	if err != nil {
 		return message.Fail("failed to parse data")
 	}
-	if err := conf.Topic.Validate(); err != nil {
+	if err := conf_topic.Validate(); err != nil {
 		return message.Fail("invalid topic: " + err.Error())
 	}
+	if conf_topic.Level() != topic.SMARTCONTRACT_LEVEL {
+		return message.Fail("topic level is not at SMARTCONTRACT LEVEL")
+	}
 
-	conf_list := parameters[3].(*key_value.List)
-	conf_key := conf.Topic.ToString(conf.Topic.Level())
+	conf_list, ok := parameters[3].(*key_value.List)
+	if !ok {
+		return message.Fail("missing configuration list")
+	}
+	conf_key := conf_topic.ToString(topic.SMARTCONTRACT_LEVEL)
 	conf_raw, err := conf_list.Get(conf_key)
 	if err != nil {
 		return message.Fail("failed to get configuration: " + err.Error())
