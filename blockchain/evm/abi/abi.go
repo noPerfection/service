8171e1745440960a08cd0b8ae72d627a1e807d67
ahmetson <ahmetson@zoho.com>
2023-04-05 19:46:39 +0300

    -- blockchain/evm/abi.Categorize (transaction decoding)

--- blockchain/evm/abi/abi.go
+++ blockchain/evm/abi/abi.go
@@ -36,48 +36,8 @@ func NewFromStatic(static_abi *static_abi.Abi) (*Abi, error) {
 	return &abi_obj, nil
 }
 
-// Given the transaction data, returns a categorized variant.
-//
-// Returns
-//   - the method name as a string.
-//   - method arguments as map of
-//     `argument name => value`
-//   - error if couldn't find decode transaction data
-func (a *Abi) Categorize(data string) (string, map[string]interface{}, error) {
-	inputs := map[string]interface{}{}
-
-	// data could be prefixed with 0x
-	offset := 0
-	if len(data) > 2 && data[:2] == "0x" || data[:2] == "0X" {
-		offset += 2
-	}
-
-	// decode method signature
-	sig, err := hex.DecodeString(data[offset : 8+offset])
-	if err != nil {
-		return "", inputs, fmt.Errorf("failed to extract method signature from transaction data. the hex package error: %w", err)
-	}
-
-	// recover Method from signature and ABI
-	method, err := a.abi.MethodById(sig)
-	if err != nil {
-		return "", inputs, fmt.Errorf("failed to find a method by its signature. geth package error: %w", err)
-	}
-
-	// decode txInput Payload
-	decoded_data, err := hex.DecodeString(data[8+offset:])
-	if err != nil {
-		return method.Name, inputs, fmt.Errorf("failed to extract method input arguments from transaction data. the hex package error: %w", err)
-	}
-
-	err = method.Inputs.UnpackIntoMap(inputs, decoded_data)
-	if err != nil {
-		return method.Name, inputs, fmt.Errorf("failed to parse method input parameters into map. the geth package error: %w", err)
-	}
-
-	return method.Name, inputs, nil
-}
-
+// Returns the list of all indexed
+// outputs of the event
 func get_indexed(inputs abi.Arguments) abi.Arguments {
 	ret := make(abi.Arguments, 0)
 	for _, arg := range inputs {
@@ -88,6 +48,12 @@ func get_indexed(inputs abi.Arguments) abi.Arguments {
 	return ret
 }
 
+// List of events by their event id
+// There could be multiple events
+// with the same id.
+//
+// however the values could be changed
+// at the indexing.
 func (a *Abi) get_events(event_id string) []abi.Event {
 	events := make([]abi.Event, 0)
 	for _, event := range a.abi.Events {
