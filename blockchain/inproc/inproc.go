deeb23dc955c177601654f752e5403df1af180f1
ahmetson <ahmetson@zoho.com>
2023-04-06 14:13:02 +0300

    Add current categorizer endpoint

--- blockchain/inproc/inproc.go
+++ blockchain/inproc/inproc.go
@@ -13,6 +13,12 @@ func ClientEndpoint(network_id string) string {
 	return "inproc://spaghetti_" + network_id
 }
 
+// Returns the current smartcontract categorizer
+// manager url
+func CurrentCategorizerEndpoint(network_id string) string {
+	return "inproc://cat_current_" + network_id
+}
+
 // Returns the old smartcontract categorizer
 // manager url
 func OldCategorizerEndpoint(network_id string) string {
@@ -22,6 +28,20 @@ func OldCategorizerEndpoint(network_id string) string {
 // Returns the categorizer manager url
 func CategorizerEndpoint(network_id string) string {
 	return "inproc://cat_" + network_id
+}
+
+func CurrentCategorizerManagerSocket(network_id string) (*zmq.Socket, error) {
+	sock, err := zmq.NewSocket(zmq.PUSH)
+	if err != nil {
+		return nil, fmt.Errorf("zmq error for new push socket: %w", err)
+	}
+
+	url := CurrentCategorizerEndpoint(network_id)
+	if err := sock.Bind(url); err != nil {
+		return nil, fmt.Errorf("trying to create categorizer for network id %s: %v", network_id, err)
+	}
+
+	return sock, nil
 }
 
 func OldCategorizerManagerSocket(network_id string) (*zmq.Socket, error) {
