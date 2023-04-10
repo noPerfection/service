28539589a24fac233095e218a8fe34005360cb11
ahmetson <ahmetson@zoho.com>
2023-04-10 17:09:24 +0300

    Rename command names to match to SQL commands

--- db/handler/handler.go
+++ db/handler/handler.go
@@ -5,38 +5,44 @@ import (
 	"fmt"
 
 	"github.com/blocklords/sds/app/command"
+	"github.com/blocklords/sds/common/data_type/key_value"
 
 	zmq "github.com/pebbe/zmq4"
 )
 
 const (
 	NEW_CREDENTIALS command.CommandName = "new-credentials" // for pull controller, to receive credentials from vault
-	READ_ROW        command.CommandName = "read-row"        // Get one row, if it doesn't exist, return error
-	READ_ALL        command.CommandName = "read"            // Read multiple line
-	WRITE           command.CommandName = "write"           // insert or update
+	SELECT_ROW      command.CommandName = "select-row"      // Get one row, if it doesn't exist, return error
+	SELECT_ALL      command.CommandName = "select"          // Read multiple line
+	INSERT          command.CommandName = "insert"          // insert new row
 	EXIST           command.CommandName = "exist"           // Returns true or false if select query has some rows
 	DELETE          command.CommandName = "delete"          // Delete some rows from database
 )
 
 // DatabaseQueryRequest has the sql and it's parameters on part with commands.
 type DatabaseQueryRequest struct {
-	Query     string        `json:"query"`             // SQL query to apply
-	Arguments []interface{} `json:"arguments"`         // Parameters to insert into SQL query
-	Outputs   []interface{} `json:"outputs,omitempty"` // For reading it will keep what kind of data parameters to get
+	// Fields to manipulate,
+	// for reading, it will have the SELECT clause fields
+	//
+	// for writing, it will have the INSERT VALUES() clause fields
+	Fields    []string      `json:"fields"`
+	Tables    []string      `json:"tables"`              // Tables that are used for query
+	Where     string        `json:"where,omitempty"`     // WHERE part of the SQL query
+	Arguments []interface{} `json:"arguments,omitempty"` // to pass in where clause
 }
 
-// ReadRowReply keeps the parameters of READ_ROW command reply by controller
-type ReadRowReply struct {
-	Outputs []interface{} `json:"outputs"` // all column parameters returned back to user
+// SelectRowReply keeps the parameters of READ_ROW command reply by controller
+type SelectRowReply struct {
+	Outputs key_value.KeyValue `json:"outputs"` // all column parameters returned back to user
 }
 
-// ReadAllReply keeps the parameters of READ_ALL command reply by controller
-type ReadAllReply struct {
-	Rows []ReadRowReply `json:"rows"` // list of rows returned back to user
+// SelectAllReply keeps the parameters of READ_ALL command reply by controller
+type SelectAllReply struct {
+	Rows []key_value.KeyValue `json:"rows"` // list of rows returned back to user
 }
 
-// WriteReply keeps the parameters of WRITE command reply by controller
-type WriteReply struct{}
+// InsertReply keeps the parameters of WRITE command reply by controller
+type InsertReply struct{}
 
 // ExistReply keeps the parameters of EXIST command reply by controller
 type ExistReply struct {
