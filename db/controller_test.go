b8a00417a6e8e102de33176e39725aa5537a09ba
ahmetson <ahmetson@zoho.com>
2023-04-10 17:10:24 +0300

    add database controller test

--- db/controller_test.go
+++ db/controller_test.go
@@ -2,13 +2,16 @@ package db
 
 import (
 	"context"
-	"fmt"
 	"path/filepath"
 	"runtime"
 	"testing"
+	"time"
 
 	"github.com/blocklords/sds/app/configuration"
 	"github.com/blocklords/sds/app/log"
+	"github.com/blocklords/sds/app/remote"
+	"github.com/blocklords/sds/app/service"
+	"github.com/blocklords/sds/db/handler"
 	"github.com/stretchr/testify/suite"
 	"github.com/testcontainers/testcontainers-go/modules/mysql"
 )
@@ -18,22 +21,19 @@ import (
 // Define the suite, and absorb the built-in basic suite
 // functionality from testify - including a T() method which
 // returns the current testing context
-type TestMysqlSuite struct {
+type TestControllerSuite struct {
 	suite.Suite
 	db_name   string
 	container *mysql.MySQLContainer
-	db_con    *Database
+	client    *remote.ClientSocket
 	ctx       context.Context
 }
 
-func (suite *TestMysqlSuite) SetupTest() {
+func (suite *TestControllerSuite) SetupTest() {
 	suite.db_name = "test"
 	_, filename, _, _ := runtime.Caller(0)
 	static_abi_sql := "20230308171023_static_abi.sql"
 	static_abi_path := filepath.Join(filepath.Dir(filename), "..", "_db", "migrations", static_abi_sql)
-
-	// create_test_db := "create_test_db.sql"
-	// create_test_db_path := filepath.Join(filepath.Dir(filename), "..", "_db", create_test_db)
 
 	ctx := context.TODO()
 	container, err := mysql.RunContainer(ctx,
@@ -47,23 +47,10 @@ func (suite *TestMysqlSuite) SetupTest() {
 	suite.container = container
 	suite.ctx = ctx
 
-	logger, err := log.New("mysql-suite", log.WITHOUT_TIMESTAMP)
+	logger, err := log.New("controller-suite", log.WITHOUT_TIMESTAMP)
 	suite.Require().NoError(err)
 	app_config, err := configuration.NewAppConfig(logger)
 	suite.Require().NoError(err)
-
-	// Getting default parameters should fail
-	// since we don't have any data set yet
-	credentials := GetDefaultCredentials(app_config)
-	suite.Require().Empty(credentials.Username)
-	suite.Require().Empty(credentials.Password)
-
-	// after settings the default parameters
-	// we should have the user name and password
-	app_config.SetDefaults(DatabaseConfigurations)
-	credentials = GetDefaultCredentials(app_config)
-	suite.Require().Equal("root", credentials.Username)
-	suite.Require().Equal("tiger", credentials.Password)
 
 	// Overwrite the host
 	host, err := container.Host(ctx)
@@ -81,58 +68,71 @@ func (suite *TestMysqlSuite) SetupTest() {
 		}
 	}
 	suite.Require().NotEmpty(exposed_port)
-	app_config.SetDefault("SDS_DATABASE_PORT", exposed_port)
+	DatabaseConfigurations.Parameters["SDS_DATABASE_PORT"] = exposed_port
+	DatabaseConfigurations.Parameters["SDS_DATABASE_NAME"] = suite.db_name
 
-	// overwrite the database name
-	app_config.SetDefault("SDS_DATABASE_NAME", suite.db_name)
-	parameters, err := GetParameters(app_config)
-	suite.Require().NoError(err)
-	suite.Require().Equal(suite.db_name, parameters.name)
+	go Run(app_config, logger)
+	// wait for initiation of the controller
+	time.Sleep(time.Second * 1)
 
-	// Connect to the database
-	suite.T().Log("open database connection by", parameters.hostname, credentials)
-	db_con, err := Open(logger, parameters, credentials)
+	database_service, err := service.Inprocess(service.DATABASE)
 	suite.Require().NoError(err)
-	suite.db_con = db_con
+	client, err := remote.InprocRequestSocket(database_service.Url(), logger, app_config)
+	suite.Require().NoError(err)
+
+	suite.client = client
 
 	suite.T().Cleanup(func() {
 		if err := container.Terminate(ctx); err != nil {
 			suite.T().Fatalf("failed to terminate container: %s", err)
 		}
-		if err := db_con.Close(); err != nil {
-			suite.T().Fatalf("failed to terminate database connection: %s", err)
+		if err := client.Close(); err != nil {
+			suite.T().Fatalf("failed to close client socket: %s", err)
 		}
 	})
 }
 
-func (suite *TestMysqlSuite) TestInsert() {
+func (suite *TestControllerSuite) TestInsert() {
+	suite.T().Log("test INSERT command")
 	// query
-	query := `INSERT INTO static_abi (abi_id, body) VALUES (?, ?)`
 	arguments := []interface{}{"test_id", `[{}]`}
-
-	_, err := suite.db_con.Query(suite.ctx, query, arguments)
+	request := handler.DatabaseQueryRequest{
+		Fields:    []string{"abi_id", "body"},
+		Tables:    []string{"static_abi"},
+		Arguments: arguments,
+	}
+	var reply handler.InsertReply
+	err := handler.INSERT.Request(suite.client, request, &reply)
 	suite.Require().NoError(err)
 
 	// query
-	query = `SELECT abi_id FROM static_abi WHERE abi_id = ?`
 	arguments = []interface{}{"test_id"}
-
-	_, err = suite.db_con.Query(suite.ctx, query, arguments)
+	request = handler.DatabaseQueryRequest{
+		Fields:    []string{"abi_id"},
+		Tables:    []string{"static_abi"},
+		Where:     "abi_id = ?",
+		Arguments: arguments,
+	}
+	var read_reply handler.SelectRowReply
+	err = handler.SELECT_ROW.Request(suite.client, request, &read_reply)
 	suite.Require().NoError(err)
-}
+	suite.Require().EqualValues("test_id", read_reply.Outputs["abi_id"])
 
-func (suite *TestMysqlSuite) TestSelect() {
+	suite.T().Log("test SELECT ALL command")
 	// query
-	query := `SELECT abi_id FROM static_abi WHERE abi_id = ?`
-	arguments := []interface{}{"test_id"}
-
-	result, err := suite.db_con.Query(suite.ctx, query, arguments)
+	request = handler.DatabaseQueryRequest{
+		Fields: []string{"abi_id", "body"},
+		Tables: []string{"static_abi"},
+	}
+	var reply_all handler.SelectAllReply
+	err = handler.SELECT_ALL.Request(suite.client, request, &reply_all)
 	suite.Require().NoError(err)
-	fmt.Println("the select result", result)
+	suite.T().Log(reply_all)
+	suite.Require().Len(reply_all.Rows, 1)
 }
 
 // In order for 'go test' to run this suite, we need to create
 // a normal test function and pass our suite to suite.Run
-func TestMysql(t *testing.T) {
-	suite.Run(t, new(TestMysqlSuite))
+func TestController(t *testing.T) {
+	suite.Run(t, new(TestControllerSuite))
 }
