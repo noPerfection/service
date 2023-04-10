1103d40037b133bd1938d4e207d54365c1ed44d3
ahmetson <ahmetson@zoho.com>
2023-04-10 11:25:16 +0300

    ++ app/service/credentials test

---
+++ app/service/credentials/credentials_test.go
@@ -0,0 +1,38 @@
+package credentials
+
+import (
+	"testing"
+
+	"github.com/blocklords/sds/app/service"
+	"github.com/stretchr/testify/suite"
+)
+
+// Define the suite, and absorb the built-in basic suite
+// functionality from testify - including a T() method which
+// returns the current testing context
+type TestServiceTypeSuite struct {
+	suite.Suite
+	service_type service.ServiceType
+}
+
+// Todo test inprocess and external types of controllers
+// Todo test the business of the controller
+// Make sure that Account is set to five
+// before each test
+func (suite *TestServiceTypeSuite) SetupTest() {
+	suite.service_type = service.BUNDLE
+}
+
+func (suite *TestServiceTypeSuite) TestVaultPath() {
+	name := vault_path(suite.service_type)
+	suite.Equal("BUNDLE_SECRET_KEY", name)
+
+	broadcast_name := vault_broadcast_path(suite.service_type)
+	suite.Equal("BUNDLE_BROADCAST_SECRET_KEY", broadcast_name)
+}
+
+// In order for 'go test' to run this suite, we need to create
+// a normal test function and pass our suite to suite.Run
+func TestServiceType(t *testing.T) {
+	suite.Run(t, new(TestServiceTypeSuite))
+}
