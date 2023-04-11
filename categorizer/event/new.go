cf2cd9a423ab2d763b2758c7b3973ac32100d6e7
ahmetson <ahmetson@zoho.com>
2023-04-11 17:57:37 +0300

    creating categorized event log from key value validates the parameters

--- categorizer/event/new.go
+++ categorizer/event/new.go
@@ -6,7 +6,17 @@ import (
 	"github.com/blocklords/sds/common/data_type/key_value"
 )
 
-// Call categorizer.NewLog().AddMetadata().AddSmartcontractData()
+// New categorized event log with the name and decoded data.
+//
+// But the metadata such as associated transaction, smartcontract
+// are not added
+//
+// Call
+//
+//	log := event.NewLog("Transfer", transfer_parameters).
+//		AddMetadata(raw_log).
+//		AddSmartcontractData(smartcontract)
+//
 // DON'T call it as a single function otherwise
 // there is no guarantee that event is valid
 func New(event_name string, parameters key_value.KeyValue) *Log {
@@ -22,6 +32,10 @@ func NewFromMap(blob key_value.KeyValue) (*Log, error) {
 	err := blob.ToInterface(&log)
 	if err != nil {
 		return nil, fmt.Errorf("failed to serialize key-value %v", err)
+	}
+
+	if err := log.Validate(); err != nil {
+		return nil, fmt.Errorf("log.Validate: %w", err)
 	}
 
 	return &log, nil
