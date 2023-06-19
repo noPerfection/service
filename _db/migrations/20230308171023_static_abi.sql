aa36b2367c67c67733f15e39900c66cb8b4722d9
ahmetson <ahmetson@zoho.com>
2023-06-19 13:48:01 +0700

    Delete all core features except the SDS Service related code

--- _db/migrations/20230308171023_static_abi.sql
+++
@@ -1,8 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-CREATE TABLE abi (
-    abi_id varchar(20) NOT NULL,
-    body json,
-    UNIQUE KEY(abi_id)
-);
--- +goose StatementEnd
--- _db/migrations/20230308174318_categorizer_smartcontract.sql
+++
@@ -1,8 +0,0 @@
--- +goose Up
-CREATE TABLE indexer_smartcontract (
-    network_id varchar(20) NOT NULL,
-	address varchar(255) NOT NULL,
-    block_number bigint UNSIGNED,
-    block_timestamp bigint UNSIGNED,
-    CONSTRAINT smartcontract_id PRIMARY KEY (network_id, address)
-);
