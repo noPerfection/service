d56d2d968e12101aa4d864103e358a8f11f22798
Medet Ahmetson <admin@blocklords.io>
2023-07-23 02:13:42 +0300

    update linting

--- service/service.go
+++ service/service.go
@@ -48,3 +48,49 @@ func PrepareProxyConfiguration(requiredProxy string, config *configuration.Confi
 
 	return nil
 }
+
+// PreparePipelineConfiguration checks that proxy url and controllerName are valid.
+// Then, in the configuration, it makes sure that dependency is linted.
+func PreparePipelineConfiguration(config *configuration.Config, proxyUrl string, controllerName string, logger *log.Logger) error {
+	//
+	// lint the dependency proxy's destination to the independent independent's controller
+	//--------------------------------------------------
+	proxyConfig, err := dev.ReadServiceConfiguration(config.Context, proxyUrl)
+	if err != nil {
+		return fmt.Errorf("dev.ReadServiceConfiguration of '%s': %w", proxyUrl, err)
+	}
+
+	destinationConfig, err := proxyConfig.GetController(proxy.DestinationName)
+	if err != nil {
+		return fmt.Errorf("getting dependency proxy's destination configuration failed: %w", err)
+	}
+
+	controllerConfig, err := config.Service.GetController(controllerName)
+	if err != nil {
+		return fmt.Errorf("getting '%s' controller from independent configuration failed: %w", controllerName, err)
+	}
+
+	// somehow it will work with only one instance. but in the future maybe another instances as well.
+	destinationInstanceConfig := destinationConfig.Instances[0]
+	instanceConfig := controllerConfig.Instances[0]
+
+	if destinationInstanceConfig.Port != instanceConfig.Port {
+		logger.Info("the dependency proxy destination not match to the controller",
+			"proxy url", proxyUrl,
+			"destination port", destinationInstanceConfig.Port,
+			"independent controller port", instanceConfig.Port)
+
+		destinationInstanceConfig.Port = instanceConfig.Port
+		destinationConfig.Instances[0] = destinationInstanceConfig
+		proxyConfig.SetController(destinationConfig)
+
+		logger.Info("linting dependency proxy's destination port", "new port", instanceConfig.Port)
+		logger.Warn("todo", 1, "if dependency proxy is running, then it should be restarted")
+		err := dev.WriteServiceConfiguration(config.Context, proxyUrl, proxyConfig)
+		if err != nil {
+			return fmt.Errorf("dev.WriteServiceConfiguration for '%s': %w", proxyUrl, err)
+		}
+	}
+
+	return nil
+}
