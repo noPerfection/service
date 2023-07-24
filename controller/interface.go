185b3cefef56acef3e73c30151fa90ee4f1b8d6c
Medet Ahmetson <admin@blocklords.io>
2023-07-24 14:40:35 +0300

    add to the controller a method to use any command with silently

--- controller/interface.go
+++ controller/interface.go
@@ -1,8 +1,13 @@
 package controller
 
 import (
+	"fmt"
+	"github.com/ahmetson/common-lib/data_type/key_value"
 	"github.com/ahmetson/service-lib/communication/command"
+	"github.com/ahmetson/service-lib/communication/message"
 	"github.com/ahmetson/service-lib/configuration"
+	"github.com/ahmetson/service-lib/log"
+	"github.com/ahmetson/service-lib/remote"
 )
 
 // Interface of the controller. All controllers have it
@@ -31,4 +36,28 @@ type Interface interface {
 	ControllerType() configuration.Type
 
 	Run() error
+}
+
+// Does nothing, simply returns the data
+var anyHandler = func(request message.Request, _ *log.Logger, _ ...*remote.ClientSocket) message.Reply {
+	replyParameters := key_value.Empty()
+	replyParameters.Set("command", request.Command)
+
+	reply := message.Reply{
+		Status:     message.OK,
+		Message:    "",
+		Parameters: replyParameters,
+	}
+	return reply
+}
+
+// AnyRoute makes the given controller as the source of the proxy.
+// It means, it will add command.Any to call the proxy.
+func AnyRoute(sourceController Interface) error {
+	route := command.NewRoute(command.Any, anyHandler)
+
+	if err := sourceController.AddRoute(route); err != nil {
+		return fmt.Errorf("failed to add any route into the controller: %w", err)
+	}
+	return nil
 }
