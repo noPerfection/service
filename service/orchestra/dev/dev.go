5af060df5729aa30e62952a84faf57cb7b86978c
Medet Ahmetson <admin@blocklords.io>
2023-08-07 00:34:21 +0300

    move ctx from config to orchestra

--- service/orchestra/dev/dev.go
+++ service/orchestra/dev/dev.go
@@ -39,15 +39,30 @@ package dev
 import (
 	"fmt"
 	"github.com/ahmetson/common-lib/data_type/key_value"
-	"github.com/ahmetson/service-lib/config/context/dev"
+	"github.com/ahmetson/service-lib/config"
 	"github.com/ahmetson/service-lib/config/env"
+	"github.com/ahmetson/service-lib/config/service"
+	"github.com/ahmetson/service-lib/os/path"
 	"github.com/ahmetson/service-lib/server"
 	"github.com/ahmetson/service-lib/service/orchestra"
+	"gopkg.in/yaml.v3"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
+const (
+	SrcKey  = "SERVICE_DEPS_SRC"
+	BinKey  = "SERVICE_DEPS_BIN"
+	DataKey = "SERVICE_DEPS_DATA"
+)
+
+// A Context handles the config of the contexts
 type Context struct {
-	config       *dev.Context
+	Src          string `json:"SERVICE_DEPS_SRC"`
+	Bin          string `json:"SERVICE_DEPS_BIN"`
+	Data         string `json:"SERVICE_DEPS_DATA"`
+	url          string
 	controller   server.Interface
 	serviceReady bool
 	deps         map[string]*Dep
@@ -74,22 +89,44 @@ func preparePath(path string) error {
 	return nil
 }
 
-// New creates an orchestra including its directories.
-func New(config orchestra.Interface) (*Context, error) {
-	if config.GetType() != orchestra.DevContext {
-		return nil, fmt.Errorf("ctx config is not a dev ctx. it's %s", config.GetType())
-	}
-	configContext, ok := config.(*dev.Context)
-	if !ok {
-		return nil, fmt.Errorf("can not convert ctx config into dev ctx")
+func GetDefaultConfigs() (*config.DefaultConfig, error) {
+	exePath, err := path.GetExecPath()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get the executable path: %w", err)
 	}
 
+	return &config.DefaultConfig{
+		Title: "Interface",
+		Parameters: key_value.Empty().
+			Set(SrcKey, path.GetPath(exePath, "./deps/.src")).
+			Set(BinKey, path.GetPath(exePath, "./deps/.bin")).
+			Set(DataKey, path.GetPath(exePath, "./deps/.data")),
+	}, nil
+}
+
+// New creates an orchestra including its directories.
+func New(conf *config.Config, url string) (*Context, error) {
+	execPath, err := path.GetExecPath()
+	if err != nil {
+		return nil, fmt.Errorf("path.GetExecPath: %w", err)
+	}
+	srcPath := path.GetPath(execPath, conf.Engine().GetString(SrcKey))
+	dataPath := path.GetPath(execPath, conf.Engine().GetString(DataKey))
+	binPath := path.GetPath(execPath, conf.Engine().GetString(BinKey))
+
 	ctx := &Context{
-		config:     configContext,
+		Src:        srcPath,
+		Bin:        binPath,
+		Data:       dataPath,
 		deps:       make(map[string]*Dep),
 		controller: nil,
 	}
-	for _, path := range config.Paths() {
+
+	if err != nil {
+		return nil, fmt.Errorf("newContext: %w", err)
+	}
+
+	for _, path := range ctx.Paths() {
 		if err := preparePath(path); err != nil {
 			return ctx, fmt.Errorf("preparePath(%s): %w", path, err)
 		}
@@ -105,12 +142,12 @@ func New(config orchestra.Interface) (*Context, error) {
 // prepareEnv writes the .env of the orchestra to share between dependencies.
 // Call it after creating a path.
 func (context *Context) prepareEnv() error {
-	kv, err := key_value.NewFromInterface(context.config)
+	kv, err := key_value.NewFromInterface(context)
 	if err != nil {
 		return fmt.Errorf("prepareEnv: %w", err)
 	}
 
-	err = env.WriteEnv(kv, context.config.EnvPath())
+	err = env.WriteEnv(kv, context.EnvPath())
 	if err != nil {
 		return fmt.Errorf("env.WriteEnv: %w", err)
 	}
@@ -138,4 +175,145 @@ func (context *Context) New(url string) (*Dep, error) {
 	dep := &Dep{url: url, context: context, cmd: nil}
 	context.deps[url] = dep
 	return dep, nil
+}
+
+func (c *Context) GetType() orchestra.Type {
+	return orchestra.DevContext
+}
+
+// GetConfig on the given path.
+// If a path is not obsolete, then it should be relative to the executable.
+// The path should have the .yml extension
+func (c *Context) GetConfig(url string) (*service.Service, error) {
+	path := c.ConfigurationPath(url)
+
+	if err := validateServicePath(path); err != nil {
+		return nil, fmt.Errorf("validateServicePath: %w", err)
+	}
+
+	bytes, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("os.ReadFile of %s: %w", path, err)
+	}
+
+	yamlConfig := createYaml()
+	kv := yamlConfig.Map()
+	err = yaml.Unmarshal(bytes, &kv)
+
+	if err != nil {
+		return nil, fmt.Errorf("yaml.Unmarshal of %s: %w", path, err)
+	}
+
+	fmt.Println("service", kv)
+
+	yamlConfig = key_value.New(kv)
+	if err := yamlConfig.Exist("Services"); err != nil {
+		return nil, fmt.Errorf("no services in yaml: %w", err)
+	}
+
+	services, err := yamlConfig.GetKeyValueList("Services")
+	if err != nil {
+		return nil, fmt.Errorf("failed to get services as key value list: %w", err)
+	}
+
+	if len(services) == 0 {
+		return nil, fmt.Errorf("no services in the config")
+	}
+
+	var serviceConfig service.Service
+	err = services[0].Interface(&serviceConfig)
+	if err != nil {
+		return nil, fmt.Errorf("convert key value to Service: %w", err)
+	}
+
+	err = serviceConfig.PrepareService()
+	if err != nil {
+		return nil, fmt.Errorf("prepareService: %w", err)
+	}
+
+	return &serviceConfig, nil
+}
+
+// WriteService writes the service as the yaml on the given path.
+// If the path doesn't contain the file extension, it will through an error
+func (c *Context) SetConfig(url string, service *service.Service) error {
+	path := c.ConfigurationPath(url)
+
+	if err := validateServicePath(path); err != nil {
+		return fmt.Errorf("validateServicePath: %w", err)
+	}
+
+	kv := createYaml(service)
+
+	serviceConfig, err := yaml.Marshal(kv.Map())
+	if err != nil {
+		return fmt.Errorf("failed to marshall config.Service: %w", err)
+	}
+
+	f, _ := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+	_, err = f.Write(serviceConfig)
+	closeErr := f.Close()
+	if err != nil {
+		return fmt.Errorf("failed to write service into the given path: %w", err)
+	} else if closeErr != nil {
+		return fmt.Errorf("failed to close the file descriptor: %w", closeErr)
+	} else {
+		return nil
+	}
+}
+
+// validateServicePath returns an error if the path is not a valid .yml link
+func validateServicePath(path string) error {
+	if len(path) < 5 || len(filepath.Base(path)) < 5 {
+		return fmt.Errorf("path is too short")
+	}
+	_, found := strings.CutSuffix(path, ".yml")
+	if !found {
+		return fmt.Errorf("the path should end with '.yml'")
+	}
+
+	return nil
+}
+
+func createYaml(configs ...*service.Service) key_value.KeyValue {
+	var services = configs
+	kv := key_value.Empty()
+	kv.Set("Services", services)
+
+	return kv
+}
+
+func (c *Context) Paths() []string {
+	return []string{c.Data, c.Bin, c.Src}
+}
+
+func (c *Context) SetUrl(url string) {
+	c.url = url
+}
+
+func (c *Context) GetUrl() string {
+	return c.url
+}
+
+func (c *Context) Host() string {
+	return "localhost"
+}
+
+//----------------------------------------------------------
+// below are the specific functions for the dev orchestra. other contexts may not have them
+//----------------------------------------------------------
+
+// EnvPath is the shared configurations between dependencies
+func (c *Context) EnvPath() string {
+	return filepath.Join(c.Data, ".env")
+}
+
+// ConfigurationPath returns config url in the orchestra's data
+func (c *Context) ConfigurationPath(url string) string {
+	fileName := config.UrlToFileName(c.GetUrl())
+	return filepath.Join(c.Data, url, fileName+".yml")
+}
+
+func (c *Context) SrcPath(url string) string {
+	return filepath.Join(c.Src, url)
 }
