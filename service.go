c23af49156847869aca5106be6ca1db9c3189fc2
Medet Ahmetson <ahmetson@zoho.com>
2023-10-02 18:59:59 +0700

    Create a function that returns a new proxy

--- service.go
+++ service.go
@@ -1,6 +1,22 @@
 // Package proxy defines the script that acts as the middleware
 package proxy
 
+import (
+	"fmt"
+	"github.com/ahmetson/service-lib"
+)
+
 // Proxy defines the parameters of the proxy service
 type Proxy struct {
+	*service.Service
+}
+
+// New proxy service returned
+func New() (*Proxy, error) {
+	independent, err := service.New()
+	if err != nil {
+		return nil, fmt.Errorf("service.New: %w", err)
+	}
+
+	return &Proxy{independent}, nil
 }
